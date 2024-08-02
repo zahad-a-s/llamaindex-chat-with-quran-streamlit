8419879510cb5dd52d865d9e0efb73edb7bd03de
ZA Syed <49692373+zahad-a-s@users.noreply.github.com>
2024-08-02 12:49:30 -0400

    context changed to condense question

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ index = load_data()
 
 if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
     st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode="context", verbose=True, streaming=True
+        chat_mode="condense_question", verbose=True, streaming=True
     )
 
 if prompt := st.chat_input(
