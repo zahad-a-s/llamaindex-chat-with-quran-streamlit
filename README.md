8e650dd415f069c073c8758d121b3a92d4bd14b4
ZA Syed <49692373+zahad-a-s@users.noreply.github.com>
2024-08-02 12:28:01 -0400

    fork

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # 🦙📚 LlamaIndex - Chat with the Streamlit docs
 
-Build a chatbot powered by LlamaIndex that augments GPT 3.5 with the contents of the Streamlit docs (or your own data).
+A modified Streamlit chatbot to chat with a Quran dataset. 
 
 ## Overview of the App
-
-<img src="app.png" width="75%">
 
 - Takes user queries via Streamlit's `st.chat_input` and displays both user queries and model responses with `st.chat_message`
 - Uses LlamaIndex to load and index data and create a chat engine that will retrieve context from that data to respond to each user query
@@ -23,10 +21,3 @@ You can get your own OpenAI API key by following the following instructions:
 ``` openai_key = <your key here> ```
    
 Alternatively, you can use [Streamlit Community Cloud's secrets management feature](https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management) to add your API key via the web interface.
-
-> [!CAUTION]
-> Don't commit your secrets file to your GitHub repository. The `.gitignore` file in this repo includes `.streamlit/secrets.toml` and `secrets.toml`. 
-
-## Try out the app
-
-Once the app is loaded, enter your question about the Streamlit library and wait for a response.
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,33 +3,31 @@ import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Quran Chatbot, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 openai.api_key = st.secrets.openai_key
+
 st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.info("Check out the [source dataset](https://www.kaggle.com/datasets/imrankhan197/the-quran-dataset) used to build this app", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Streamlit's open-source Python library!",
+            "content": "Assalamu Alaikum. Ask me anything about the Holy Quran.",
         }
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+    reader = SimpleDirectoryReader(
+    input_files=["./data/Kaggle_Quran_Dataset_test.csv"]
+)
     docs = reader.load_data()
     Settings.llm = OpenAI(
         model="gpt-3.5-turbo",
         temperature=0.2,
-        system_prompt="""You are an expert on 
-        the Streamlit Python library and your 
-        job is to answer technical questions. 
-        Assume that all questions are related 
-        to the Streamlit Python library. Keep 
-        your answers technical and based on 
-        facts – do not hallucinate features.""",
+        system_prompt="""You are an expert in the Quran. Answer all queries in an academic but friendly manner. Always cite references from the Quran (with surah Enlglish name and number, followed by verse(s)) whenever possible.""",
     )
     index = VectorStoreIndex.from_documents(docs)
     return index
@@ -39,7 +37,7 @@ index = load_data()
 
 if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
     st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode="condense_question", verbose=True, streaming=True
+        chat_mode="context", verbose=True, streaming=True
     )
 
 if prompt := st.chat_input(
